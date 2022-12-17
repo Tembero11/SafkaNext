45116b512958ba09332e3214c3a9313726d9ca69
Tembero11 <silver.teemu@gmail.com>
2022-12-18 00:36:49 +0200

    Meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/png" href="/favicon.png" />
         <meta name="theme-color" content="#ff3535" />
+        <meta name="description" content="What's on the menu today?" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Safka Online" />
+        <meta property="og:description" content="What's on the menu today?" />
+        {/* <meta property="og:image" content="" /> */}
       </Head>
       <body>
         <Main />
