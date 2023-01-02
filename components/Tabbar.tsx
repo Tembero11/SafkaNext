a8be9e9d67879f6fcac51dea949fc8f7572f95ec
Tembero <silver.teemu@gmail.com>
2023-01-02 11:26:27 +0200

    Clean tabbar now shows up on small touch screen devices

--- components/Tabbar.tsx
+++ components/Tabbar.tsx
@@ -6,17 +6,16 @@ interface ITabbarProps {
 }
 
 export default function Tabbar(props: ITabbarProps) {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0.0000001);
   const [scrollPosition, setScrollPosition] = useState(0);
   const tabsEl = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    setCurrentPage(0);
+  }, [])
+
   return (
     <div className={styles.container}>
-      <div className={styles.tabNames}>
-        { props.children.map((e, index) => <h5 style={{
-          left: (tabsEl.current?.offsetWidth || 0) * -currentPage + (tabsEl.current?.offsetWidth || 0)  * index
-        }} key={index}>This is tab {index + 1}</h5>) }
-      </div>
       <div className={styles.tabs} ref={tabsEl} onScroll={event => {
         if (!tabsEl.current) return;
 
@@ -26,13 +25,35 @@ export default function Tabbar(props: ITabbarProps) {
         setCurrentPage(currentPage);
         setScrollPosition(scrollPosition);
       }}>
-        { props.children }
+        {props.children.map((tabContent, index) => {
+          return (
+            <div key={index} className={styles.tab}>
+              <div className={styles.tabBox} style={{
+                transform: `scale(${calcTabSize(index, currentPage)}) translateX(${calcTabPosition(index, currentPage)}px)`,
+              }}>
+                {tabContent}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
 }
 
-
+function calcTabSize(pageIndex: number, currentPage: number) {
+  let result = 1 - Math.abs(pageIndex - currentPage);
+  if (result > 1) result = 1;
+  if (result < 0.7) result = 0.7;
+  return result;
+}
+function calcTabPosition(pageIndex: number, currentPage: number) {
+  const tabbarWidth = typeof window !== "undefined" ? window.innerWidth : 160;
+  if (pageIndex > currentPage) {
+    return -(pageIndex - currentPage) * (tabbarWidth * .4);
+  }
+  return 0;
+}
 
 interface ITabProps {
   children: React.ReactNode
@@ -41,9 +62,9 @@ interface ITabProps {
 
 export function Tab(props: ITabProps) {
   return (
-    <div className={styles.tab}>
+    <>
       <h4>{props.name}</h4>
       {props.children}
-    </div>
+    </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Tabbar, { Tab } from "../components/Tabbar";
@@ -7,6 +8,12 @@ import Week from "../components/Week";
 import getWeekMenu, { WeekMenu } from "../utils/getWeekMenu";
 
 const Home: NextPage<{ menu: WeekMenu | null }> = ({menu}) => {
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    setMobile(isMobileDevice());
+  }, [])
+
   return (
       <>
         <Head>
@@ -14,24 +21,37 @@ const Home: NextPage<{ menu: WeekMenu | null }> = ({menu}) => {
         </Head>
         <Navbar/>
         <div className={"content"}>
-          {/* <Week menu={menu}/> */}
-          <Tabbar>
-            <Tab name={"Tab1"}>
-              <p>I&apos;m tab 1</p>
-            </Tab>
-            <Tab name={"Tab2"}>
-              <p>I&apos;m tab 2</p>
-            </Tab>
-            <Tab name={"Tab3"}>
-              <p>I&apos;m tab 3</p>
-            </Tab>
-          </Tabbar>
+          {
+            isMobile ? (
+              <Tabbar>
+                <Tab name={"Tab1"}>
+                  <p>I&apos;m tab 1</p>
+                </Tab>
+                <Tab name={"Tab2"}>
+                  <p>I&apos;m tab 2</p>
+                </Tab>
+                <Tab name={"Tab3"}>
+                  <p>I&apos;m tab 3</p>
+                </Tab>
+              </Tabbar>
+            ) : (
+              <Week menu={menu}/>
+            )
+          }
         </div>
         <Footer/>
       </>
   );
 }
 
+function isMobileDevice() {
+  if (typeof window == "undefined") return false;
+
+  const isTouch = "ontouchstart" in window;
+  const isSmallScreen = window.innerWidth < 550;
+
+  return isTouch && isSmallScreen;
+}
 
 Home.getInitialProps = async () => {
   try {
